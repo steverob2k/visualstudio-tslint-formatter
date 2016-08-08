632472286c32a7840857ab4ae8af2c01535c38df
Roberts <sroberts@ingrnet.com>
2016-08-08 13:54:34 +0100

    bugs

--- formatters/visualstudioErrorFormatter.ts
+++ formatters/visualstudioErrorFormatter.ts
@@ -1,20 +1,20 @@
 import * as Lint from "tslint/lib/lint";
 
-export class VisualstudioErrorFormatter extends Lint.Formatters.AbstractFormatter {
+export class Formatter extends Lint.Formatters.AbstractFormatter {
 
   public format(failures: Lint.RuleFailure[]): string {
-    return failures.map(this.formatFailure).join("\n") + "\n";
+    return failures.map(this.formatFailure).join("\n") + "\n \n";
   }
 
   private formatFailure(failure: Lint.RuleFailure): string {
-    let fileName = failure.getFileName();
+    let filePath = ("src/app/" + failure.getFileName()).toString().replace(/\//g, "\\");
     let lineCharNos = failure.getStartPosition().getLineAndCharacter();
     let lineNo = lineCharNos.line;
     let characterNo = lineCharNos.character;
     let message = failure.getFailure();
     let ruleName = failure.getRuleName();
 
-    return fileName + "(" + (lineNo) + "," + (characterNo) + "): error " + ruleName + ": " + message;
+    return filePath + "(" + (lineNo + 1) + "," + (characterNo + 1) + "): error " + ruleName + ": " + message;
 
   }
 

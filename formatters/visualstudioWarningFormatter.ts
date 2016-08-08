234a236a3e08326c103ba7519b6bffcf3e99c405
steverob2k <steverob2k@hotmail.co.uk>
2016-08-08 11:17:20 +0100

    initial code

---
+++ formatters/visualstudioWarningFormatter.ts
@@ -0,0 +1,21 @@
+import * as Lint from "tslint/lib/lint";
+
+export class VisualstudioWarningFormatter extends Lint.Formatters.AbstractFormatter {
+
+  public format(failures: Lint.RuleFailure[]): string {
+    return failures.map(this.formatFailure).join("\n") + "\n";
+  }
+
+  private formatFailure(failure: Lint.RuleFailure): string {
+    let fileName = failure.getFileName();
+    let lineCharNos = failure.getStartPosition().getLineAndCharacter();
+    let lineNo = lineCharNos.line;
+    let characterNo = lineCharNos.character;
+    let message = failure.getFailure();
+    let ruleName = failure.getRuleName();
+
+    return fileName + "(" + (lineNo) + "," + (characterNo) + "): warning " + ruleName + ": " + message;
+
+  }
+
+}
